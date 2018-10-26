252e180aa5432b1c1b23cea99c48ebf466612b14
Luis Valdez <lvaldez@attivio.com>
2018-10-26 16:41:08 -0400

    PLAT-42612: First steps toward custom facets.

--- src/components/Facet.js
+++ src/components/Facet.js
@@ -1,256 +1,60 @@
 // @flow
 import React from 'react';
-import PropTypes from 'prop-types';
 
-import SearchFacet from '../api/SearchFacet';
-import SearchFacetBucket from '../api/SearchFacetBucket';
-import DateUtils from '../util/DateUtils';
-import DateFormat from '../util/DateFormat';
 import Card from './Card';
 import CollapsiblePanel from './CollapsiblePanel';
-import BarChartFacetContents from './BarChartFacetContents';
-import PieChartFacetContents from './PieChartFacetContents';
-import MoreListFacetContents from './MoreListFacetContents';
-import ListWithBarsFacetContents from './ListWithBarsFacetContents';
-import TagCloudFacetContents from './TagCloudFacetContents';
-import TimeSeriesFacetContents from './TimeSeriesFacetContents';
-import SentimentFacetContents from './SentimentFacetContents';
-import MapFacetContents from './MapFacetContents';
-import SentimentTagCloudFacetContents from './SentimentTagCloudFacetContents';
-import FacetSearchBar from './FacetSearchBar';
-
-export type FacetType = 'barchart' | 'columnchart' | 'piechart' | 'barlist' |
-  'tagcloud' | 'timeseries' | 'list' | 'sentiment' | 'geomap' |
-  (facetName: string) => React$Element<*>;
 
 type FacetProps = {
-  /** The facet to display. */
-  facet: SearchFacet;
-  /** The facet for positive key phrases used in Sentiment TagCloud */
-  positiveKeyphrases?: SearchFacet;
-  /** The facet for negative key phrases used in Sentiment TagCloud */
-  negativeKeyphrases?: SearchFacet;
-  /** The way the facet information should be displayed. Defaults to 'list' */
-  type: FacetType;
   /**
-   * The maximum number of items to show in a facet. If there
-   * are more than this many buckets for the facet, only this many, with
-   * the highest counts, will be shown. Defaults to 15.
+   * The component to display as the facet's contents
    */
-  maxBuckets: number;
+  contents: any;
+  /**
+   * The name to display for the facet.
+   */
+  name: string;
+  /**
+   * The name of the field or the field expression the facet is displaying.
+   */
+  field: string;
   /**
    * If set, then the facet will be displayed in a collapsible component. If
    * the facet is collapsible and has no buckets, it will be collapsed initially.
    */
   collapse: boolean;
-  /** If set, then the facet will be displayed in a card which has a border around it */
+  /**
+   * If set, then the facet will be displayed in a card which has a border around it
+   */
   bordered: boolean;
-  /** Controls the colors used to show various entity types (the value can be any valid CSS color) */
-  entityColors: Map<string, string>;
 }
 
 type FacetDefaultProps = {
-  type: FacetType;
-  maxBuckets: number;
   collapse: boolean;
   bordered: boolean;
-  entityColors: Map<string, string>;
 };
 
 /**
- * Display a single facet from the search results.
+ * Display a single facet.
  */
 export default class Facet extends React.Component<FacetDefaultProps, FacetProps, void> {
   static defaultProps = {
-    type: 'list',
-    maxBuckets: 15,
     collapse: false,
     bordered: false,
-    entityColors: new Map(),
-  };
-
-  static contextTypes = {
-    searcher: PropTypes.any,
   };
 
   static displayName = 'Facet';
 
-  constructor(props: FacetProps) {
-    super(props);
-    (this: any).addFacetFilter = this.addFacetFilter.bind(this);
-    (this: any).addTimeSeriesFilter = this.addTimeSeriesFilter.bind(this);
-  }
-
-  addFacetFilter(bucket: SearchFacetBucket | any, customBucketLabel: ?string) {
-    let bucketLabel;
-    if (this.props.positiveKeyphrases || this.props.negativeKeyphrases) {
-      bucketLabel = customBucketLabel || bucket.value.displayLabel();
-      if (bucket.sentiment === 'positive' && this.props.positiveKeyphrases) {
-        this.context.searcher.addFacetFilter(this.props.positiveKeyphrases.findLabel(), bucketLabel, bucket.value.filter);
-      } else if (bucket.sentiment === 'negative' && this.props.negativeKeyphrases) {
-        this.context.searcher.addFacetFilter(this.props.negativeKeyphrases.findLabel(), bucketLabel, bucket.value.filter);
-      }
-    } else if (this.props.facet) {
-      bucketLabel = customBucketLabel || bucket.displayLabel();
-      this.context.searcher.addFacetFilter(this.props.facet.findLabel(), bucketLabel, bucket.filter);
-    }
-  }
-
-  /**
-   * Create a facet filter for the starting and ending dates...
-   * If start is set but end is not, filters on the specific time
-   * set by start, otherwises filters on the range. (If start is not
-   * set, this simply returns).
-   */
-  addTimeSeriesFilter(start: Date | null, end: Date | null) {
-    if (start !== null) {
-      let facetFilterString;
-      let labelString;
-
-      const startFacetFilterString = DateUtils.formatDate(start, DateFormat.ATTIVIO);
-      const startLabelString = DateUtils.formatDate(start, DateFormat.MEDIUM);
-      if (end !== null) {
-        const endFacetFilterString = DateUtils.formatDate(end, DateFormat.ATTIVIO);
-        const endLabelString = DateUtils.formatDate(end, DateFormat.MEDIUM);
-        labelString = `${startLabelString} to ${endLabelString}`;
-
-        facetFilterString = `${this.props.facet.name}:FACET(RANGE("${startFacetFilterString}", "${endFacetFilterString}", upper=inclusive))`; // eslint-disable-line max-len
-      } else {
-        labelString = startLabelString;
-        facetFilterString = `${this.props.facet.name}:FACET(RANGE("${startFacetFilterString}", ${startFacetFilterString}, upper=inclusive))`; // eslint-disable-line max-len
-      }
-      this.context.searcher.addFacetFilter(this.props.facet.findLabel(), labelString, facetFilterString);
-    }
-  }
-
   render() {
-    const facetColors = this.props.entityColors;
-    let facetColor;
-    if (this.props.facet) {
-      facetColor = facetColors.has(this.props.facet.field) ? facetColors.get(this.props.facet.field) : null;
-    }
-
-    let facetContents;
-    if (this.props.type === 'sentimenttagcloud' && this.props.positiveKeyphrases && this.props.negativeKeyphrases) {
-      if (this.props.positiveKeyphrases.buckets && this.props.negativeKeyphrases.buckets) {
-        if (this.props.positiveKeyphrases.buckets.length > 0 || this.props.negativeKeyphrases.buckets.length > 0) {
-          facetContents = (
-            <SentimentTagCloudFacetContents
-              positiveBuckets={this.props.positiveKeyphrases.buckets}
-              negativeBuckets={this.props.negativeKeyphrases.buckets}
-              maxBuckets={this.props.maxBuckets}
-              addFacetFilter={this.addFacetFilter}
-            />
-          );
-        }
-      }
-    }
-
-    if (this.props.facet && this.props.facet.buckets && this.props.facet.buckets.length > 0) {
-      switch (this.props.type) {
-        case 'barchart':
-          facetContents = facetColor ? (
-            <BarChartFacetContents
-              buckets={this.props.facet.buckets}
-              addFacetFilter={this.addFacetFilter}
-              color={facetColor}
-            />
-          ) : (
-            <BarChartFacetContents
-              buckets={this.props.facet.buckets}
-              addFacetFilter={this.addFacetFilter}
-            />
-            );
-          break;
-        case 'columnchart':
-          facetContents = facetColor ? (
-            <BarChartFacetContents
-              buckets={this.props.facet.buckets}
-              addFacetFilter={this.addFacetFilter}
-              columns
-              color={facetColor}
-            />
-          ) : (
-            <BarChartFacetContents
-              buckets={this.props.facet.buckets}
-              addFacetFilter={this.addFacetFilter}
-              columns
-            />
-            );
-          break;
-        case 'piechart':
-          facetContents = (
-            <PieChartFacetContents
-              buckets={this.props.facet.buckets}
-              addFacetFilter={this.addFacetFilter}
-              entityColors={this.props.entityColors}
-            />
-          );
-          break;
-        case 'barlist':
-          facetContents = facetColor ? (
-            <ListWithBarsFacetContents
-              buckets={this.props.facet.buckets}
-              addFacetFilter={this.addFacetFilter}
-              color={facetColor}
-            />
-          ) : (
-            <ListWithBarsFacetContents
-              buckets={this.props.facet.buckets}
-              addFacetFilter={this.addFacetFilter}
-            />
-          );
-          break;
-        case 'tagcloud':
-          facetContents = (
-            <TagCloudFacetContents
-              buckets={this.props.facet.buckets}
-              maxBuckets={this.props.maxBuckets}
-              addFacetFilter={this.addFacetFilter}
-            />
-          );
-          break;
-        case 'timeseries':
-          facetContents = <TimeSeriesFacetContents buckets={this.props.facet.buckets} addFacetFilter={this.addTimeSeriesFilter} />;
-          break;
-        case 'sentiment':
-          facetContents = <SentimentFacetContents buckets={this.props.facet.buckets} addFacetFilter={this.addFacetFilter} />;
-          break;
-        case 'geomap':
-          facetContents = <MapFacetContents buckets={this.props.facet.buckets} addFacetFilter={this.addFacetFilter} />;
-          break;
-        case 'list':
-        default: {
-          facetContents = (
-            <FacetSearchBar
-              name={this.props.facet.field}
-              label={this.props.facet.label}
-              addFacetFilter={this.addFacetFilter}
-            >
-              <MoreListFacetContents buckets={this.props.facet.buckets} addFacetFilter={this.addFacetFilter} />
-            </FacetSearchBar>
-          );
-          break;
-        }
-      }
-    } else if (!this.props.positiveKeyphrases && !this.props.negativeKeyphrases) {
-      facetContents = <span className="none">No values for this facet.</span>;
-    }
-
-    // Prefer the display name but fall back to the name field
-    const label = this.props.facet ? this.props.facet.findLabel() : '';
-
-    if (this.props.facet && this.props.collapse) {
-      const collapsed = this.props.facet.buckets.length === 0;
+    if (this.props.collapse) {
       return (
-        <CollapsiblePanel title={label} id={`facet-${this.props.facet.field}`} collapsed={collapsed}>
-          {facetContents}
+        <CollapsiblePanel title={this.props.name} id={`facet-${this.props.field}`}>
+          {this.props.contents}
         </CollapsiblePanel>
       );
     }
     return (
-      <Card title={label} borderless={!this.props.bordered} className="attivio-facet">
-        {facetContents}
+      <Card title={this.props.name} borderless={!this.props.bordered} className="attivio-facet">
+        {this.props.contents}
       </Card>
     );
   }
--- src/components/FacetResults.js
+++ src/components/FacetResults.js
@@ -4,6 +4,33 @@ import PropTypes from 'prop-types';
 
 import Facet from './Facet';
 import SearchFacet from '../api/SearchFacet';
+import SearchFacetBucket from '../api/SearchFacetBucket';
+import DateUtils from '../util/DateUtils';
+import DateFormat from '../util/DateFormat';
+import BarChartFacetContents from './BarChartFacetContents';
+import PieChartFacetContents from './PieChartFacetContents';
+import MoreListFacetContents from './MoreListFacetContents';
+import ListWithBarsFacetContents from './ListWithBarsFacetContents';
+import TagCloudFacetContents from './TagCloudFacetContents';
+import TimeSeriesFacetContents from './TimeSeriesFacetContents';
+import SentimentFacetContents from './SentimentFacetContents';
+import MapFacetContents from './MapFacetContents';
+import SentimentTagCloudFacetContents from './SentimentTagCloudFacetContents';
+import FacetSearchBar from './FacetSearchBar';
+
+export type FacetDisplayType = 'piechart' | 'barchart' | 'columnchart' | 'barlist' |
+  'tagcloud' | 'timeseries' | 'sentiment' |'geomap' | 'list';
+
+/**
+ * This is the definition of a facet renderer. It is passed a facet from
+ * the search results, the facet's position in the list, and the type, if any,
+ * from the lists of facet types in the configuration.
+ * It should return either a React component that will be rendered as the
+ * facet's contents or null if the facet shouldn't be rendered by this function.
+ * If no FacetContentsRenderer functions handle the rendering, then the facet
+ * will be rendered by one of the default facet content types.
+ */
+export type FacetContentsRenderer = (facet: SearchFacet, position: number, type: FacetDisplayType) => any;
 
 type FacetResultsProps = {
   /** The facet field names that should be displayed as pie charts */
@@ -42,6 +69,16 @@ type FacetResultsProps = {
    * buckets. By default, facets with no buckets will be hidden.
    */
   showEmptyFacets: boolean;
+  /**
+   * Any custom facet renderers you want to use. These will be callled, in order,
+   * to render the contents of each facet. If a renderer renders the facet, the
+   * subsequent ones won't be called. If no renderers are given, or if none
+   * of the passed in renderers can or wants to render the facet, the default
+   * behaviour will occur where the facet will be rendered based on the "type"
+   * lists (pieChartFacets, barChartFacets, etc.), eventually defaulting to list
+   * facets.
+   */
+  renderers: Array<FacetContentsRenderer>;
 };
 
 type FacetResultsDefaultProps = {
@@ -57,6 +94,7 @@ type FacetResultsDefaultProps = {
   orderHint: Array<string>;
   entityColors: Map<string, string>;
   showEmptyFacets: boolean;
+  renderers: Array<FacetContentsRenderer>;
 };
 
 /**
@@ -81,6 +119,7 @@ export default class FacetResults extends React.Component<FacetResultsDefaultPro
     orderHint: [],
     entityColors: new Map(),
     showEmptyFacets: false,
+    renderers: [],
   };
 
   static contextTypes = {
@@ -99,7 +138,168 @@ export default class FacetResults extends React.Component<FacetResultsDefaultPro
     return false;
   }
 
-  getFacetDisplayType(field: string) {
+  constructor(props: FacetResultsProps) {
+    super(props);
+    (this: any).addFacetFilter = this.addFacetFilter.bind(this);
+    (this: any).addTimeSeriesFilter = this.addTimeSeriesFilter.bind(this);
+  }
+
+  addFacetFilter(bucket: SearchFacetBucket | any, customBucketLabel: ?string) {
+    let bucketLabel;
+    if (this.props.positiveKeyphrases || this.props.negativeKeyphrases) {
+      bucketLabel = customBucketLabel || bucket.value.displayLabel();
+      if (bucket.sentiment === 'positive' && this.props.positiveKeyphrases) {
+        this.context.searcher.addFacetFilter(this.props.positiveKeyphrases.findLabel(), bucketLabel, bucket.value.filter);
+      } else if (bucket.sentiment === 'negative' && this.props.negativeKeyphrases) {
+        this.context.searcher.addFacetFilter(this.props.negativeKeyphrases.findLabel(), bucketLabel, bucket.value.filter);
+      }
+    } else if (this.props.facet) {
+      bucketLabel = customBucketLabel || bucket.displayLabel();
+      this.context.searcher.addFacetFilter(this.props.facet.findLabel(), bucketLabel, bucket.filter);
+    }
+  }
+
+  /**
+   * Create a facet filter for the starting and ending dates...
+   * If start is set but end is not, filters on the specific time
+   * set by start, otherwises filters on the range. (If start is not
+   * set, this simply returns).
+   */
+  addTimeSeriesFilter(start: Date | null, end: Date | null) {
+    if (start !== null) {
+      let facetFilterString;
+      let labelString;
+
+      const startFacetFilterString = DateUtils.formatDate(start, DateFormat.ATTIVIO);
+      const startLabelString = DateUtils.formatDate(start, DateFormat.MEDIUM);
+      if (end !== null) {
+        const endFacetFilterString = DateUtils.formatDate(end, DateFormat.ATTIVIO);
+        const endLabelString = DateUtils.formatDate(end, DateFormat.MEDIUM);
+        labelString = `${startLabelString} to ${endLabelString}`;
+
+        facetFilterString = `${this.props.facet.name}:FACET(RANGE("${startFacetFilterString}", "${endFacetFilterString}", upper=inclusive))`; // eslint-disable-line max-len
+      } else {
+        labelString = startLabelString;
+        facetFilterString = `${this.props.facet.name}:FACET(RANGE("${startFacetFilterString}", ${startFacetFilterString}, upper=inclusive))`; // eslint-disable-line max-len
+      }
+      this.context.searcher.addFacetFilter(this.props.facet.findLabel(), labelString, facetFilterString);
+    }
+  }
+
+  renderBuiltInFacetType(facet: SearchFacet, type: FacetDisplayType): any {
+    const facetColor = this.props.entityColors.has(facet.field) ? this.props.entityColors.get(facet.field) : null;
+    let facetContents;
+
+    switch (type) {
+      case 'barchart':
+        facetContents = facetColor ? (
+          <BarChartFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addFacetFilter}
+            color={facetColor}
+          />
+        ) : (
+          <BarChartFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addFacetFilter}
+          />
+          );
+        break;
+      case 'columnchart':
+        facetContents = facetColor ? (
+          <BarChartFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addFacetFilter}
+            columns
+            color={facetColor}
+          />
+        ) : (
+          <BarChartFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addFacetFilter}
+            columns
+          />
+          );
+        break;
+      case 'piechart':
+        facetContents = (
+          <PieChartFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addFacetFilter}
+            entityColors={this.props.entityColors}
+          />
+        );
+        break;
+      case 'barlist':
+        facetContents = facetColor ? (
+          <ListWithBarsFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addFacetFilter}
+            color={facetColor}
+          />
+        ) : (
+          <ListWithBarsFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addFacetFilter}
+          />
+          );
+        break;
+      case 'tagcloud':
+        facetContents = (
+          <TagCloudFacetContents
+            buckets={facet.buckets}
+            maxBuckets={this.props.maxFacetBuckets}
+            addFacetFilter={this.addFacetFilter}
+          />
+        );
+        break;
+      case 'timeseries':
+        facetContents = (
+          <TimeSeriesFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addTimeSeriesFilter}
+          />
+        );
+        break;
+      case 'sentiment':
+        facetContents = (
+          <SentimentFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addFacetFilter}
+          />
+        );
+        break;
+      case 'geomap':
+        facetContents = (
+          <MapFacetContents
+            buckets={facet.buckets}
+            addFacetFilter={this.addFacetFilter}
+          />
+        );
+        break;
+      case 'list':
+      default: {
+        facetContents = (
+          <FacetSearchBar
+            name={facet.field}
+            label={facet.label}
+            addFacetFilter={this.addFacetFilter}
+          >
+            <MoreListFacetContents buckets={facet.buckets} addFacetFilter={this.addFacetFilter} />
+          </FacetSearchBar>
+        );
+        break;
+      }
+    }
+    return facetContents;
+  }
+
+  /**
+   * Determine the type to use when displaying a facet based on the configuration's lists of
+   * facet names for each type. Custom renderers are free to ignore the type when displaing
+   * a facet that they know about...
+   */
+  getFacetDisplayType(field: string): FacetDisplayType {
     if (FacetResults.matchesFacetList(field, this.props.pieChartFacets)) {
       return 'piechart';
     }
@@ -135,6 +335,38 @@ export default class FacetResults extends React.Component<FacetResultsDefaultPro
     return false;
   }
 
+  findFacetContents(facet: SearchFacet, index: number): any {
+    let result = null;
+    if (this.shouldShow(facet)) {
+      const type = this.getFacetDisplayType(facet.field);
+      // Look through the registered rendering functions to find one who can render it
+      for (let i = 0; i < this.props.renderers.length; i += 1) {
+        const renderer = this.props.renderers[i];
+        result = renderer(facet, index, type);
+        if (result) {
+          break;
+        }
+      }
+      if (!result) {
+        result = renderBuiltInFacetType(facet, type);
+      }
+    }
+    return result;
+  }
+
+  pushFacetToResults(facets: Array<any>, facet: SearchFacet, contents: any) {
+    // Prefer the display name but fall back to the name field
+    const name = facet.findLabel();
+    facets.push((
+      <Facet
+        name={name}
+        field={facet.field}
+        contents={facetContents}
+        collapse
+      />
+    ));
+  }
+
   renderFacets() {
     const searcher = this.context.searcher;
     const facets = searcher.state.response ? searcher.state.response.facets : null;
@@ -144,33 +376,20 @@ export default class FacetResults extends React.Component<FacetResultsDefaultPro
         facetsMap.set(facet.name, facet);
       });
       const results = [];
+      let i = 0;
       this.props.orderHint.forEach((facetName) => {
         const facet = facetsMap.get(facetName);
-        if (facet && this.shouldShow(facet)) {
-          const type = this.getFacetDisplayType(facet.field);
-          results.push(<Facet
-            facet={facet}
-            type={type}
-            key={facet.name}
-            maxBuckets={this.props.maxFacetBuckets}
-            collapse
-            entityColors={this.props.entityColors}
-          />);
+        if (facet) {
+          i += 1;
+          const facetContents = this.findFacetContents(facet, i);
+          this.pushFacetToResults(results, facet, facetContents);
         }
       });
       facets.forEach((facet: SearchFacet) => {
         if (!this.props.orderHint.includes(facet.name)) {
-          if (this.shouldShow(facet)) {
-            const type = this.getFacetDisplayType(facet.field);
-            results.push(<Facet
-              facet={facet}
-              type={type}
-              key={facet.name}
-              maxBuckets={this.props.maxFacetBuckets}
-              collapse
-              entityColors={this.props.entityColors}
-            />);
-          }
+          i += 1;
+          const facetContents = this.findFacetContents(facet, i);
+          this.pushFacetToResults(results, facet, facetContents);
         }
       });
       return results;
@@ -179,10 +398,27 @@ export default class FacetResults extends React.Component<FacetResultsDefaultPro
   }
 
   render() {
+    const facets = this.renderFacets();
+  
     return (
       <div className="facetResults">
-        {this.renderFacets()}
+        {facets}
       </div>
     );
   }
 }
+
+// if (this.props.type === 'sentimenttagcloud' && this.props.positiveKeyphrases && this.props.negativeKeyphrases) {
+//   if (this.props.positiveKeyphrases.buckets && this.props.negativeKeyphrases.buckets) {
+//     if (this.props.positiveKeyphrases.buckets.length > 0 || this.props.negativeKeyphrases.buckets.length > 0) {
+//       facetContents = (
+//         <SentimentTagCloudFacetContents
+//           positiveBuckets={this.props.positiveKeyphrases.buckets}
+//           negativeBuckets={this.props.negativeKeyphrases.buckets}
+//           maxBuckets={this.props.maxBuckets}
+//           addFacetFilter={this.addFacetFilter}
+//         />
+//       );
+//     }
+//   }
+// }
